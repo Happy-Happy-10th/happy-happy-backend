041eb807efc683bef603c40d05a1cf4e870ca46d
moyeon <vryehdus0528@gmail.com>
2025-07-18 17:10:54 +0900

    feat: CommonException, MemberDetails 클래스 추가

---
+++ src/main/java/com/happyhappy/backend/common/exception/CommonException.java
@@ -0,0 +1,20 @@
+package com.happyhappy.backend.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CommonException extends RuntimeException {
+
+    private final String code;
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    public CommonException(String code, String message, HttpStatus httpStatus) {
+        super(message);
+        this.code = code;
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+}
