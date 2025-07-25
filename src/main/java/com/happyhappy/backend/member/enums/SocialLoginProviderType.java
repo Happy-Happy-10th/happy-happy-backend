faef2824661f9e3808653c2973df6b86253b8570
moyeon <vryehdus0528@gmail.com>
2025-07-25 22:16:17 +0900

    feat: Google OAuth 2.0 로그인 구현
    - Oauth2 인증 플로우 구현
    - 일반 로그인과 소셜 로그인 분리 처리
    - 로그인 콜백 URL 리다이렉트 구현

---
+++ src/main/java/com/happyhappy/backend/member/enums/SocialLoginProviderType.java
@@ -0,0 +1,23 @@
+package com.happyhappy.backend.member.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum SocialLoginProviderType {
+    GOOGLE("google");
+
+    private final String registrationId;
+
+    SocialLoginProviderType(String registrationId) {
+        this.registrationId = registrationId;
+    }
+
+    public static SocialLoginProviderType fromRegistrationId(String registrationId) {
+        for (SocialLoginProviderType type : SocialLoginProviderType.values()) {
+            if (type.getRegistrationId().equals(registrationId)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Unknown registration id:" + registrationId);
+    }
+}
