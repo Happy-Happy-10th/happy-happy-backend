57d7257129f81a3d92510ba1e2c0cecb20e6c02d
seungwon <rmfltmskatls@naver.com>
2025-07-29 18:43:38 +0900

    implement logout

---
+++ src/main/java/com/happyhappy/backend/authentication/controller/LogoutController.java
@@ -0,0 +1,24 @@
+package com.happyhappy.backend.authentication.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogoutController {
+    @PostMapping("/auth/logout")
+    public void logout(HttpServletResponse response) {
+        Cookie del = new Cookie("refreshToken", null);
+        del.setHttpOnly(true);
+        // HTTPS 배포 시 변경
+        // del.setSecure(true);
+        del.setPath("/");
+        del.setMaxAge(0);
+        response.addCookie(del);
+    }
+}
+
+
+
+
