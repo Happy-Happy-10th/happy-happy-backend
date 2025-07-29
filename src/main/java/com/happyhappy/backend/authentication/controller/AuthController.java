806e0621c008fc8658836b79de9ab50ef674a457
seungwon <rmfltmskatls@naver.com>
2025-07-29 19:54:51 +0900

    create refresh token & modify logout API

--- src/main/java/com/happyhappy/backend/authentication/controller/AuthController.java
+++ src/main/java/com/happyhappy/backend/authentication/controller/AuthController.java
@@ -1,14 +1,23 @@
 package com.happyhappy.backend.authentication.controller;
 
+import com.happyhappy.backend.authentication.provider.TokenProvider;
 import com.happyhappy.backend.member.dto.MemberDto.LoginRequest;
 import com.happyhappy.backend.member.dto.MemberDto.LoginResponse;
 import com.happyhappy.backend.member.service.MemberService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.management.remote.JMXAuthenticator;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,10 +25,42 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final MemberService memberService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenProvider tokenProvider;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest ,
+                                               HttpServletResponse response) {
+
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String refreshToken = tokenProvider.generateRefreshToken(authentication);
+
+        Cookie refreshCookie = new Cookie("refreshToken", refreshToken);
+        refreshCookie.setHttpOnly(true);
+        refreshCookie.setPath("/");
+        refreshCookie.setMaxAge(24 * 60 * 60);
+        response.addCookie(refreshCookie);
+
 
         return ResponseEntity.ok(memberService.login(loginRequest));
     }
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignupResponse> signup(@RequestBody SignupRequest request) {
+        SignupResponse response = memberService.signup(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED) // 201 Created
+                .body(response);
+    }
+
+
 }
