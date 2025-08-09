b03b4fd2bf89daf910ac855aab11944da2b8d043
peter <gusdnr598@naver.com>
2025-08-09 20:39:07 +0900

    fix: 프롬프트 수정

--- src/main/java/com/happyhappy/backend/common/config/SecurityConfig.java
+++ src/main/java/com/happyhappy/backend/common/config/SecurityConfig.java
@@ -84,6 +84,7 @@ public class SecurityConfig {
                         .requestMatchers("/error/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html")
                         .permitAll()
+                        .requestMatchers("/chat/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter(),
