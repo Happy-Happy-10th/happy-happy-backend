043b9a72f71a9fc1a621004e66397953d0ef685e
seungwon <rmfltmskatls@naver.com>
2025-08-24 17:30:36 +0900

    fix: Logout https 환경수정

--- src/main/java/com/happyhappy/backend/authentication/controller/LogoutController.java
+++ src/main/java/com/happyhappy/backend/authentication/controller/LogoutController.java
@@ -1,19 +1,23 @@
 package com.happyhappy.backend.authentication.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Logout")
 @RestController
 public class LogoutController {
+
+    @Operation(summary = "로그아웃", description = "로그아웃을 합니다.")
     @PostMapping("/logout")
     public ResponseEntity<String> logout(HttpServletResponse response) {
         Cookie del = new Cookie("refreshToken", null);
         del.setHttpOnly(true);
-        // HTTPS 배포 시 변경
-        // del.setSecure(true);
+        del.setSecure(true); // HTTPS 사용
         del.setPath("/");
         del.setMaxAge(0);
         response.addCookie(del);
